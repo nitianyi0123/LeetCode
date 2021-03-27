8b070643aefa449858b4a121b44deeab2cdeeb78
倪天熠 <nitianyi@shouqianba.com>
2021-03-27 15:53:51 +0800

    61

---
+++ src/leetcode/editor/cn/RotateList.java
@@ -0,0 +1,85 @@
+package leetcode.editor.cn;
+
+//给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5], k = 2
+//输出：[4,5,1,2,3]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [0,1,2], k = 4
+//输出：[2,0,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 500] 内 
+// -100 <= Node.val <= 100 
+// 0 <= k <= 2 * 109 
+// 
+// Related Topics 链表 双指针 
+// 👍 500 👎 0
+
+
+public class RotateList {
+
+    public static void main(String[] args) {
+	    Solution solution = new RotateList().new Solution();
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null) {
+            return null;
+        }
+        ListNode leader = head;
+        int len = 0;
+        while (head != null) {
+            len++;
+            if (head.next == null) {
+                head.next = leader;
+                break;
+            }
+            head = head.next;
+        }
+        for (int i = 0; i < len - (k % len); i++) {
+            head = head.next;
+        }
+        leader = head.next;
+        head.next = null;
+        return leader;
+    }
+}
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
